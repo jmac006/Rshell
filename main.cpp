a8d29fa3e0cbc6f526de0e334997152e76e7f98f
Justin Mac <jmac006@ucr.edu>
2016-05-10 17:07:37 -0700

    Added main file

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <unistd.h> //used for fork(), exec()
+#include <sys/types.h> //used for wait
+#include <sys/wait.h> //used for wait
+
+using namespace std;
+
+int main () {
+	pid_t pid = fork();
+	if (pid == -1) {
+		perror("fork");
+	}
+	if (pid == 0) {
+		cout << "Child: " << pid << endl;
+	}
+	if (pid > 0) {
+		/*if (wait(0) == -1) {
+			perror("wait");
+		}*/
+		cout << "Parent: " << pid << endl;
+	}
+	return 0;
+}
